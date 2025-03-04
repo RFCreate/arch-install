8850e43109373cf89401e35cf7a64a7a2fded71c
RFCreate <107062289+RFCreate@users.noreply.github.com>
2025-03-04 11:23:49 -0600

    fix: change return for exit

--- iso2usb.sh
+++ iso2usb.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
 # Check if mkfs commands exists
-! command -v mkfs.fat > /dev/null 2>&1 && echo "Error: Dependency mkfs.fat command is not available" && return 1
-! command -v mkfs.ext4 > /dev/null 2>&1 && echo "Error: Dependency mkfs.ext4 command is not available" && return 1
+! command -v mkfs.fat > /dev/null 2>&1 && echo "Error: Dependency mkfs.fat command is not available" && exit 1
+! command -v mkfs.ext4 > /dev/null 2>&1 && echo "Error: Dependency mkfs.ext4 command is not available" && exit 1
 
 # Exit if USB is invalid
-[ -z "$USB" ] && echo "Error: Missing USB device, use USB=/dev/your_usb" && return 1
-[ ! -b "$USB" ] && echo "Error: USB does not exist, use USB=/dev/your_usb" && return 1
-[ "$(lsblk -dno type "$USB")" != "disk" ] && echo "Error: USB is not disk type, use USB=/dev/your_usb" && return 1
+[ -z "$USB" ] && echo "Error: Missing USB device, use USB=/dev/your_usb" && exit 1
+[ ! -b "$USB" ] && echo "Error: USB does not exist, use USB=/dev/your_usb" && exit 1
+[ "$(lsblk -dno type "$USB")" != "disk" ] && echo "Error: USB is not disk type, use USB=/dev/your_usb" && exit 1
 
 # Exit if ISO is invalid
-[ -z "$ISO" ] && echo "Error: Missing ISO file, use ISO=path/to/archlinux-version-x86_64.iso" && return 1
-[ ! -f "$ISO" ] && echo "Error: ISO file does not exist, use ISO=path/to/archlinux-version-x86_64.iso" && return 1
-! bsdtar -t -f "$ISO" > /dev/null 2>&1 && echo "Error: Unrecognized archive format, use ISO=path/to/archlinux-version-x86_64.iso" && return 1
+[ -z "$ISO" ] && echo "Error: Missing ISO file, use ISO=path/to/archlinux-version-x86_64.iso" && exit 1
+[ ! -f "$ISO" ] && echo "Error: ISO file does not exist, use ISO=path/to/archlinux-version-x86_64.iso" && exit 1
+! bsdtar -t -f "$ISO" > /dev/null 2>&1 && echo "Error: Unrecognized archive format, use ISO=path/to/archlinux-version-x86_64.iso" && exit 1
 
 # Remove partition signatures
 echo "Removing disk signatures..."
-wipefs --all -q "${USB}" || ! echo "Error ocurred!" || return 1
+wipefs --all -q "${USB}" || ! echo "Error ocurred!" || exit 1
 
 # Partition usb
 echo "Partitioning disk..."
-printf "size=+2G,type=L,bootable,\nsize=+,type=L\n" | sfdisk -q "${USB}" || ! echo "Error ocurred!" || return 1
+printf "size=+2G,type=L,bootable,\nsize=+,type=L\n" | sfdisk -q "${USB}" || ! echo "Error ocurred!" || exit 1
 
 ############ ISO ############
 
