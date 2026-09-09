a5f66d9e9e40051b0cfac36eaa269263038873a7
RFCreate <107062289+RFCreate@users.noreply.github.com>
2026-09-09 17:32:05 -0600

    feat: use helper function to reduce large pacman options

--- install.sh
+++ install.sh
@@ -7,6 +7,11 @@ CONSOLE_FONT="ter-122b"
 KEYBOARD_MAP="la-latin1"
 TIMEZONE="Etc/GMT+6"
 
+# Helper function to install packages with logging
+pacman_install() {
+    pacman -S --needed --noconfirm "$@" 2>&1 | tee -a /pacman.log
+}
+
 # https://wiki.archlinux.org/title/Installation_guide#Time
 # Set time zone
 ln -sf "/usr/share/zoneinfo/$TIMEZONE" /etc/localtime
@@ -26,7 +31,7 @@ echo "KEYMAP=$KEYBOARD_MAP" > /etc/vconsole.conf
 
 # https://wiki.archlinux.org/title/Linux_console#Persistent_configuration
 # Iinstall console font
-pacman -S --needed --noconfirm "$FONT_PACKAGE" 2>&1 | tee -a /pacman.log
+pacman_install "$FONT_PACKAGE"
 # Set console font
 echo "FONT=$CONSOLE_FONT" >> /etc/vconsole.conf
 
@@ -34,12 +39,12 @@ echo "FONT=$CONSOLE_FONT" >> /etc/vconsole.conf
 # Set hostname for network
 echo arch > /etc/hostname
 # Install and enable network manager
-pacman -S --needed --noconfirm networkmanager network-manager-applet 2>&1 | tee -a /pacman.log
+pacman_install networkmanager network-manager-applet
 systemctl --quiet enable NetworkManager.service
 
 # https://wiki.archlinux.org/title/Installation_guide#Boot_loader
 if [ -f /sys/firmware/efi/fw_platform_size ]; then
-    pacman -S --needed --noconfirm grub efibootmgr 2>&1 | tee -a /pacman.log
+    pacman_install grub efibootmgr
     # https://wiki.archlinux.org/title/Installation_guide#Verify_the_boot_mode
     case "$(cat /sys/firmware/efi/fw_platform_size)" in
         # https://wiki.archlinux.org/title/GRUB#Installation
@@ -48,7 +53,7 @@ if [ -f /sys/firmware/efi/fw_platform_size ]; then
     esac
 else
     # https://wiki.archlinux.org/title/GRUB#Installation_2
-    pacman -S --needed --noconfirm grub 2>&1 | tee -a /pacman.log
+    pacman_install grub
     cd / && grub-install --target=i386-pc "$(findmnt --output source --noheadings --target . | sed 's/[0-9]*$//')"
 fi
 # Set time for grub menu
@@ -58,12 +63,12 @@ grub-mkconfig -o /boot/grub/grub.cfg
 
 # https://wiki.archlinux.org/title/Microcode
 # Install processor microcode update
-grep -q AuthenticAMD /proc/cpuinfo && pacman -S --needed --noconfirm amd-ucode 2>&1 | tee -a /pacman.log
-grep -q GenuineIntel /proc/cpuinfo && pacman -S --needed --noconfirm intel-ucode 2>&1 | tee -a /pacman.log
+grep -q AuthenticAMD /proc/cpuinfo && pacman_install amd-ucode
+grep -q GenuineIntel /proc/cpuinfo && pacman_install intel-ucode
 
 # https://wiki.archlinux.org/title/Broadcom_wireless#Driver_selection
 # Install Broadcom drivers if needed
-[ -n "$(lspci -d 14e4:)" ] && pacman -S --needed --noconfirm broadcom-wl 2>&1 | tee -a /pacman.log
+[ -n "$(lspci -d 14e4:)" ] && pacman_install broadcom-wl
 
 # https://wiki.archlinux.org/title/PC_speaker#Globally
 # Remove beep sound
